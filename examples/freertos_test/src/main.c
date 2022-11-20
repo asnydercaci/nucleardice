b2ebb7e7625703aacb3b3502be3abcad01802a1b
mjwaxios <8632495+mjwaxios@users.noreply.github.com>
2022-11-20 15:30:46 -0500

    changed tick to pvParams

--- examples/freertos_test/src/main.c
+++ examples/freertos_test/src/main.c
@@ -1,30 +1,35 @@
 #include <driver/gpio.h>
-// Include FreeRTOS for delay
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-#define LED 2 // LED connected to GPIO2
+// If not defined in the platformio.ini then define it here
+#ifndef LED
+#define LED (2) 
+#endif
 
 void led_blink(void *pvParams) {
+   TickType_t t = (*(int *) pvParams) / portTICK_RATE_MS;
     while (1) {
         gpio_set_level(LED,0);
-        vTaskDelay(1000/portTICK_RATE_MS);
+        vTaskDelay(t);
         gpio_set_level(LED,1);
-        vTaskDelay(1000/portTICK_RATE_MS);
+        vTaskDelay(t);
     }
 }
 
 void led_blinky2(void *pvParams) {
+   TickType_t t = (*(int *) pvParams) / portTICK_RATE_MS;
     while (1) {
         gpio_set_level(LED,0);
-        vTaskDelay(888/portTICK_RATE_MS);
+        vTaskDelay(t);
     }
 }
 
 void led_blinky3(void *pvParams) {
+   TickType_t t = (*(int *) pvParams) / portTICK_RATE_MS;
     while (1) {
         gpio_set_level(LED,1);
-        vTaskDelay(1111/portTICK_RATE_MS);
+        vTaskDelay(t);
     }
 }
 
@@ -39,9 +44,14 @@ int app_main() {
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
-    xTaskCreate(&led_blink,"LED_BLINK",512,NULL,5,NULL);
-    xTaskCreate(&led_blinky2,"LED_BLINK2",512,NULL,7,NULL);
-    xTaskCreate(&led_blinky3,"LED_BLINK3",512,NULL,5,NULL);
+    int t = 1000;
+    xTaskCreate(&led_blink,"LED_BLINK",512,&t,5,NULL);
+
+    t = 888;
+    xTaskCreate(&led_blinky2,"LED_BLINK2",512,&t,7,NULL);
+
+    t = 1111;
+    xTaskCreate(&led_blinky3,"LED_BLINK3",512,&t,5,NULL);
 
     return 0;
 }
